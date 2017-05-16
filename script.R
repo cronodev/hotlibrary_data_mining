251f3989a90f1fe6d5400b252f73f0c82eeb71c7
Caio <caiof.adriano@hotmail.com>
2017-05-16 10:51:00 -0300

    Base files

---
+++ script.R
@@ -0,0 +1,7 @@
+setwd("c:/Users/19846/Desktop/data_mining/")
+#====================Importação=======================
+#=====================================================
+#=====================================================
+require("ggplot2")
+
+result = read.csv(file = "Sistema HotLibrary - Alunos (respostas) - Respostas ao formulário 1.csv", header = T, sep = ",", dec = ".", encoding = "UTF-8")
