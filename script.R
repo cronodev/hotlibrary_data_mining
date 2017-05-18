d218609e66bc98081b9ef73b994ff3be0a745e04
Caio <caiof.adriano@hotmail.com>
2017-05-18 13:50:48 -0300

    first graph

--- script.R
+++ script.R
@@ -1,7 +1,13 @@
-setwd("c:/Users/19846/Desktop/data_mining/")
+setwd("c:/Users/19846/Desktop/hotlibrary_data_mining/") # Coloque o caminho para o projeto.
 #====================Importação=======================
 #=====================================================
 #=====================================================
 require("ggplot2")
 
-result = read.csv(file = "Sistema HotLibrary - Alunos (respostas) - Respostas ao formulário 1.csv", header = T, sep = ",", dec = ".", encoding = "UTF-8")
+result = read.csv(file = "data.csv", header = T, sep = ",", dec = ".", encoding = "UTF-8")
+
+info = result[result$UsaBiblioteca == 'Sim',]
+
+# Frequencia de uso da biblioteca
+ggplot(info, aes(x = info$frequencia)) + geom_bar() + labs(title = "Frequencia de uso da biblioteca") + xlab('Frequencia') + ylab('')
+
